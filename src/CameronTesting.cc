fc5016629757aa5d289fb1fb219da77af9ccb3b3
cameronosmith <cameronsmithbusiness@gmail.com>
2019-01-21 20:11:46 -0800

    removed tokenize from ReadData and moved samplecsv, still need to merge data and readdata

--- src/CameronTesting.cc
+++ src/CameronTesting.cc
@@ -5,7 +5,7 @@ int main () {
     //testing readData compatability
     NamedData container;
     int val = 0;
-    DataFrame df ("SampleCSV.csv",val);;
+    DataFrame df ("../data/SampleCSV.csv",val);;
     std::cout<<df<<std::endl;
     std::cout << df("hi",0)<<std::endl<<std::flush;
 
--- src/ReadData.cc
+++ src/ReadData.cc
@@ -5,7 +5,6 @@
 #include "Common.h"
 #include <fstream>
 
-#define DELIM ","
 #define VALID_DOUBLE_CHARS "1234567890.,"
 
 /* method to read in data from csv into underlying dataframe structure
@@ -15,19 +14,21 @@
  * @return: true if read was successful, false if not
  * */
 bool ReadData (std::string filepath, NamedData & container) {
+    //the separator for the parsing
+    const std::string DELIM =  ",";
     //the file stream for the csv file
     std::ifstream file (filepath);    
     //to hold the text from each line in the csv and every word in line
     std::string currentLine;
-    char* currentWord;
+    //to hold a container of split words and specifically the named columns
+    std::vector < std::string > splitWords;
+    std::vector < std::string > namesContainer;
     //to hold num representation
     double wordVal;
     //to hold the number of columns in the data
     int numCols;
     //to hold the data for more efficient inserts before dumping in container
     std::vector <std::vector < double > > numericContainer;
-    //to hold the possible column names in the csv file header
-    std::vector < std::string > namesContainer;
 
     // check validity of file 
     if ( !file.is_open() )
@@ -35,18 +36,18 @@ bool ReadData (std::string filepath, NamedData & container) {
 
     //get first line to check whether has column names or not
     std::getline(file, currentLine);
-    //if col names process, if not put back first line
+    //get each col name from header and add to column name container
+    splitWords = SplitString (currentLine, DELIM);
+    //if col names present process them, if not put back first line
     if (currentLine.find_first_not_of(VALID_DOUBLE_CHARS)!=std::string::npos) {
-        //get each col name from header and add to column name container
-        currentWord = strtok(&currentLine[0], DELIM);
-        while (currentWord) {
-            namesContainer.push_back(currentWord);
-            currentWord = strtok(NULL, DELIM);
-            numCols++;
-        }
+        namesContainer = splitWords;
+        numCols += splitWords.size();
     }
-    else 
+    else {
         file.seekg(0, std::ios::beg);
+        for (int idx = 0; idx < splitWords.size(); idx++)
+            namesContainer.push_back(COL_PREFIX+std::to_string(idx));
+    }
     currentLine.clear();
 
     //setup container with num vecs to hold just numeric csv data
@@ -56,17 +57,17 @@ bool ReadData (std::string filepath, NamedData & container) {
     /* parse every line in csv file to extract data */
     while ( std::getline(file, currentLine) ) {
         //parse/tokenize line to get every word and add to vectors
-        currentWord = strtok(&currentLine[0], DELIM);
-        for (int colIdx = 0; currentWord && colIdx < numCols; colIdx++ ) {
+        splitWords = SplitString (currentLine, DELIM);
+        for (int colIdx = 0; colIdx < namesContainer.size(); colIdx++ ) {
             #ifdef READ_DATA_DEBUG
-                std::cout<<std::stod(currentWord)<<std::endl;
+                std::cout<<splitWords[colIdx]<<std::endl;
                 std::cout<<"size of vec is now "<<
                     numericContainer[colIdx].size()<<std::endl;
             #endif
             //could sanity check but expensive
             //push to appropriate vec and reset
-            numericContainer[colIdx].push_back(std::stod(currentWord));
-            currentWord = strtok(NULL, DELIM);
+                double parsedInt = std::stod(splitWords[colIdx]);
+                numericContainer.at(colIdx).push_back(parsedInt);
         }
     }
 
