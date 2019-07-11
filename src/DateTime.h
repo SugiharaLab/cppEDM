d58f0405299c755dcda558a1d5ccfcf2438cac2c
cameronosmith <cos008@ucsd.edu>
2019-07-11 12:19:56 -0700

    there was a weird bug i couldn't find in fixing the style of the last update, so i just copied the old datetime files and restyled them without the bug

--- src/DateTime.h
+++ src/DateTime.h
@@ -1,19 +1,19 @@
 #ifndef DATETIMEUTIL_H
 #define DATETIMEUTIL_H
 
-#include <iostream> // for testing
-#include <cstdio>   // time formatting
+#include <iostream>//for testing
+#include <cstdio> //time formatting
 #include <ctime> 
 #include <string> 
 #include <bits/stdc++.h> 
 #include <regex> 
 
-const int iso_start_year = 1900;
-const int iso_start_month = 1;
+const int iso_start_year    = 1900;
+const int iso_start_month   = 1;
 
-struct datetime_info { struct tm   time;
-                       std::string datetime_fmt;
-                       bool        unrecognized_fmt; };
+struct datetime_info { struct tm    time = {};
+                       std::string  datetime_fmt;
+                       bool         unrecognized_fmt; };  
 
 // Prototypes
 void parse_datetime_str ( struct tm & time_obj, 
@@ -25,5 +25,6 @@ datetime_info parse_datetime ( std::string datetime );
 std::string increment_datetime_str ( std::string datetime1, 
                                      std::string datetime2,
                                      int         tp );
+
 
 #endif
