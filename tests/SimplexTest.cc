fb9d018265458bf4d054771d91ff16e656a109a4
SoftwareLiteracy <JosephPark@SoftwareLiteracyFoundation.org>
2020-11-11 11:16:34 -0500

    v1.7.0 Cleanup & Tests.

--- tests/SimplexTest.cc
+++ tests/SimplexTest.cc
@@ -10,46 +10,35 @@ int main () {
 
     //----------------------------------------------------------
     // Simplex prediction on block_3sp data : No dynamic embedding
-    // Load pyEDM output from:
-    //   ./Predict.py -i block_3sp.csv -r x_t -c x_t y_t z_t
-    //   -e -l 1 99 -p 100 198 -T 1 -P -o Smplx_embd_block_3sp_pyEDM.csv
     //----------------------------------------------------------
     pyOutput = DataFrame < double >("./data/","Smplx_embd_block_3sp_pyEDM.csv");
-    
+
     // Generate cpp output
     cppOutput = Simplex ( "../data/", "block_3sp.csv",
                           "./data/", "Smplx_embd_block_3sp_cppEDM.csv",
                           "1 99","100 198", 3, 1, 0, -1, 0,
                           "x_t y_t z_t", "x_t", true, false, false );
     // Comparison
-    MakeTest ( "Simplex: block_3sp.csv embedded data test",
+    MakeTest ( "Simplex: block_3sp.csv embedded data",
                pyOutput, cppOutput );
 
     //----------------------------------------------------------
     // Simplex prediction with dynamically embedded data
-    // Load pyEDM output from:
-    //   ./Predict.py -i block_3sp.csv -r x_t -c x_t -E 3
-    //   -l 1 100 -p 101 195 -T 1 -P -o Smplx_E3_block_3sp_pyEDM.csv
     //----------------------------------------------------------
     pyOutput = DataFrame < double > ("./data/","Smplx_E3_block_3sp_pyEDM.csv");
-    
+
     // Generate cpp output
     cppOutput = Simplex ( "../data/", "block_3sp.csv",
                           "./data/", "Smplx_E3_block_3sp_cppEDM.csv",
                           "1 100", "101 195", 3, 1, 0, -1, 0,
                           "x_t", "x_t", false, false, false );
     // Comparison
-    MakeTest ( "Simplex: block_3sp.csv dynamic embedding test",
+    MakeTest ( "Simplex: block_3sp.csv dynamic embedding",
                pyOutput, cppOutput );
 
-    
     //-------------------------------------------------------------------
     // Simplex prediction with dynamically embedded data and ISO Datetime
-    // Load pyEDM output from (Datetime converted to day since 0000-12-30)
-    //   Predict.py -pa ../data/ -i "S12CD-S333-SumFlow_1980-2005.csv"
-    //              -l 1 800 -p 801 1375 -c "S12.C.D.S333" -r "S12.C.D.S333"
-    //              -E 3 -T 1 -P -v -o Smplx_S12CD_E3_pyEDM.csv
-    //----------------------------------------------------------
+    //-------------------------------------------------------------------
     pyOutput = DataFrame < double > ("./data/","Smplx_S12CD_E3_pyEDM.csv");
 
     // Generate cpp output
@@ -61,7 +50,45 @@ int main () {
     cppOutput.MaxRowPrint() = 5; // Set number of rows to print
 
     // std::cout << cppOutput; // ISO datetime in Time column
-    
+
     MakeTest ( "Simplex: S12CD-S333 ISO datetime", pyOutput, cppOutput );
 
+    //----------------------------------------------------------
+    // Simplex Ties
+    //----------------------------------------------------------
+    pyOutput = DataFrame < double > ("./data/","Smplx_ties_valid.csv");
+
+    // Generate cpp output
+    cppOutput = Simplex ( "./data/", "tie.csv",
+                          "./data/", "Smplx_ties_cppEDM.csv",
+                          "1 10", "1 10", 1, 1, 0, -1, 0,
+                          "ts", "ts", false, false, false );
+    // Comparison
+    MakeTest ( "Simplex: neighbor ties", pyOutput, cppOutput );
+
+    //----------------------------------------------------------
+    // Simplex Ties 2
+    //----------------------------------------------------------
+    pyOutput = DataFrame < double > ("./data/","Smplx_ties2_valid.csv");
+
+    // Generate cpp output
+    cppOutput = Simplex ( "./data/", "tserr.csv",
+                          "./data/", "Smplx_ties2_cppEDM.csv",
+                          "1 100", "10 15", 1, 1, 0, -1, 0,
+                          "ts", "ts", false, false, false );
+    // Comparison
+    MakeTest ( "Simplex: neighbor ties 2", pyOutput, cppOutput );
+
+    //----------------------------------------------------------
+    // Simplex disjoint lib
+    //----------------------------------------------------------
+    pyOutput = DataFrame < double > ("./data/","Smplx_disjointLib_valid.csv");
+
+    // Generate cpp output
+    cppOutput = Simplex ( "./data/", "small_data.csv",
+                          "./data/", "Smplx_disjointLib_cppEDM.csv",
+                          "1 3 6 12", "1 12", 0, 1, 0, -1, 0,
+                          "x y", "x", true, false, false );
+    // Comparison
+    MakeTest ( "Simplex: disjoint library", pyOutput, cppOutput );
 }
