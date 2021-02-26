2be011c41b2a321e722b82bd06a65c889ef50249
SoftwareLiteracy <JosephPark@SoftwareLiteracyFoundation.org>
2021-02-26 12:37:23 -0500

    v1.8.0 Remove deletion of partial embedding rows. Add deletePartial argument to MakeBlock.  Refactor library index creation. Fix disjoint library bug.

--- etc/embed.cc
+++ etc/embed.cc
@@ -8,25 +8,27 @@
 //----------------------------------------------------------------
 int main( int argc, char *argv[] ) {
 
-    std::string dataFile   = "../data/block_3sp.csv";
-    std::string columns    = "x_t";
-    std::string fileOut    = "embed_out.csv";
-    int         E          = 3;
-    int         tau        = -1; 
-    bool        verbose    = false;     // 'y' = true
-    
+    std::string dataFile      = "../data/block_3sp.csv";
+    std::string columns       = "x_t";
+    std::string fileOut       = "embed_out.csv";
+    int         E             = 3;
+    int         tau           = -1;
+    bool        deletePartial = false;  // 'y' = true
+    bool        verbose       = false;  // 'y' = true
+
     if ( argc > 1 ) { dataFile  = argv[1]; }
     if ( argc > 2 ) { columns   = argv[2]; }
     if ( argc > 3 ) { fileOut   = argv[3]; }
-    if ( argc > 4 ) { std::stringstream ss( argv[4] ); ss >> E;        }    
-    if ( argc > 5 ) { std::stringstream ss( argv[5] ); ss >> tau;      }
-    if ( argc > 6 ){ verbose    = ( *argv[6] == 'y' ? true : false );  }
+    if ( argc > 4 ) { std::stringstream ss( argv[4] ); ss >> E;           }    
+    if ( argc > 5 ) { std::stringstream ss( argv[5] ); ss >> tau;         }
+    if ( argc > 6 ) { deletePartial = ( *argv[6] == 'y' ? true : false ); }
+    if ( argc > 7 ) { verbose       = ( *argv[7] == 'y' ? true : false ); }
 
     if ( verbose ) {
         std::cout << dataFile << " cols " << columns
                   << " E " << E << " tau " << tau << std::endl;
     }
-    
+
     try {
         //----------------------------------------------------------
         //----------------------------------------------------------
@@ -48,8 +50,19 @@ int main( int argc, char *argv[] ) {
         if ( fileOut.size() ) {
             dataFrame.WriteData( "", fileOut );
         }
+
+        // MakeBlock
+        DataFrame<double> data( "../data/", "circle.csv" );
+        
+        DataFrame<double> embedding = MakeBlock( std::ref( data ), E, tau,
+                                                 data.ColumnNames(),
+                                                 deletePartial );
+        if ( verbose ) {
+            embedding.MaxRowPrint() = embedding.NRows();
+            std::cout << embedding;
+        }
     }
-    
+
     catch ( const std::exception& e ) {
  	std::cout << "Exception caught in main:\n";
         std::cout << e.what() << std::endl;
@@ -61,6 +74,6 @@ int main( int argc, char *argv[] ) {
     }
 
     std::cout << "normal termination\n";
-    
+
     return 0;
 }
